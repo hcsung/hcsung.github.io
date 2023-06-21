5e461b18f551a21b2971c8c348e9f9c778985066
Hsiao Chien Sung <shawnhcsung@gmail.com>
2023-06-21 23:34:18 +0800

    update

---
+++ animated_bg.js
@@ -0,0 +1,65 @@
+class AnimatedBackground {
+
+	delay = [];
+	duration = [];
+	left = [];
+
+	constructor(count) {
+
+		// count: number of squares
+
+		let diff =  100 / count;
+
+		// initialize
+		for (let i = 0; i < count; i++) {
+			this.delay.push((i * 2) % 30);
+			this.duration.push((i * 3) % 30 + 20);
+			this.left.push(i * diff - 5);
+		}
+
+		// randomize
+		this.delay = this.shake(this.delay, count);
+		this.duration = this.shake(this.duration, count);
+		this.left = this.shake(this.left, count);
+
+		this.draw(count);
+	}
+
+	rand(min, max) {
+		return Math.floor(Math.random() * (max - min) + min);
+	}
+
+	shake(arr, count) {
+		for (let i = 0; i < 3 * count; i++) {
+			let a = this.rand(0, arr.length);
+			let b = this.rand(0, arr.length);
+			let temp = arr[a];
+			arr[a] = arr[b];
+			arr[b] = temp;
+		}
+		return arr;
+	}
+
+	draw(count) {
+
+		let ul = document.createElement('ul');
+		let size = 1;
+
+		for (let i = 0; i < count; i++) {
+			let li = document.createElement('li');
+			size += (i % 2);
+			size = size % 8;
+			li.style.width = size + 'rem';
+			li.style.height = size + 'rem';
+			li.style.bottom = -size + 'rem';
+			li.style.animationDelay = this.delay[i] + 's';
+			li.style.animationDuration = this.duration[i] + 's';
+			li.style.left = this.left[i] + '%';
+			ul.appendChild(li);
+		}
+		ul.classList.add('bg');
+		document.body.appendChild(ul);
+	}
+}
+
+new AnimatedBackground(12);
---
+++ third_party/typing_effect.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2023 by Coding Journey (https://codepen.io/Coding_Journey/pen/BEMgbX)
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+const textArray = ["hard.", "fun!", "a journey.", "love <3"];
+
+const typedTextSpan = document.querySelector(".typed");
+const cursorSpan = document.querySelector(".cursor");
+
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+	if (charIndex < textArray[textArrayIndex].length) {
+		if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+		typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+		charIndex++;
+		setTimeout(type, typingDelay);
+	}
+	else {
+		cursorSpan.classList.remove("typing");
+		setTimeout(erase, newTextDelay);
+	}
+}
+
+function erase() {
+	if (charIndex > 0) {
+		if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+		typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+		charIndex--;
+		setTimeout(erase, erasingDelay);
+	}
+	else {
+		cursorSpan.classList.remove("typing");
+		textArrayIndex++;
+		if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+		setTimeout(type, typingDelay + 1100);
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
+	if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
